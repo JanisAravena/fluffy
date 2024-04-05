813b2a141b4cdaf5e39264258aa622198a5d47cf
LilDie <neobad.reyes@gmail.com>
2024-04-05 15:01:09 -0300

    arreglos y cambios para comenzar

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
-
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router/routes';
-import Main from './main'; 
+import './App.css'
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <RouterProvider router={router}>
-        <Main />
-      </RouterProvider>
-    </BrowserRouter>
-  );
-};
 
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
