9946bfb8fc2325370dc6420b82180b1f2308faaf
LilDie <neobad.reyes@gmail.com>
2024-04-05 20:21:31 -0300

    fuentes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router/routes';
-import './App.css'
+import './App.css';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
